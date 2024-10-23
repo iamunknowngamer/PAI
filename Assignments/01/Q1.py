d6f962479f5ab17396298b9ad620a78dbca318f4
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-10-23 08:56:08 +0500

    Create Q1.py

---
+++ Assignments/01/Q1.py
@@ -0,0 +1,78 @@
+class Animal:
+    def __init__(self, name, age, habitat):
+        self.name = name
+        self.age = age
+        self.habitat = habitat
+        self.availability = True 
+
+    def isAvailable(self, is_available):
+        self.availability = is_available
+
+    def Display(self):
+        pass
+
+
+class Mammal(Animal):
+    def __init__(self, name, age, habitat, furLen, diet):
+        super().__init__(name, age, habitat)
+        self.furLen = furLen
+        self.diet = diet
+
+    def Display(self):
+        print(f"Mammal Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Habitat: {self.habitat}")
+        print(f"Fur Length: {self.furLen}")
+        print(f"Diet Type: {self.diet}")
+        print(f"Availability: {self.availability}\n")
+
+
+class Bird(Animal):
+    def __init__(self, name, age, habitat, wingspan, flightAltitude):
+        super().__init__(name, age, habitat)
+        self.wingspan = wingspan
+        self.flightAltitude = flightAltitude
+
+    def Display(self):
+        print(f"Bird Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Habitat: {self.habitat}")
+        print(f"Wingspan: {self.wingspan}")
+        print(f"Flight Altitude: {self.flightAltitude}")
+        print(f"Availability: {self.availability}\n")
+
+
+class Reptile(Animal):
+    def __init__(self, name, age, habitat, scalePattern, isVenomous):
+        super().__init__(name, age, habitat)
+        self.scalePattern = scalePattern
+        self.isVenomous = isVenomous
+
+    def Display(self):
+        venom = "Venomous" if self.isVenomous else "Non-Venomous"
+        print(f"Reptile Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Habitat: {self.habitat}")
+        print(f"Scale Pattern: {self.scalePattern}")
+        print(f"Venom Status: {venom}")
+        print(f"Availability: {self.availability}\n")
+
+
+#Driver Code
+
+dolphin = Mammal(name="Dolphin", age=11, habitat="Pacific Ocean", furLen="Medium", diet="Carnivore")
+hawk = Bird(name="Hawk", age=7, habitat="Mountains", wingspan="3m", flightAltitude="3000m")
+snake = Reptile(name="Snake", age=2, habitat="Desert", scalePattern="Striped", isVenomous=True)
+
+dolphin.Display()
+hawk.Display()
+snake.Display()
+
+dolphin.isAvailable(False)
+hawk.isAvailable(True)
+snake.isAvailable(False)
+
+print("------------------After updating------------------\n")
+dolphin.Display()
+hawk.Display()
+snake.Display()
