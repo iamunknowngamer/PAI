5561b6a94e7a46a9ede7f1c7cd75e5f123d29552
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-11-25 09:15:11 +0500

    Update Q6.py

--- Lab_09/Lab_Tasks/Q6.py
+++ Lab_09/Lab_Tasks/Q6.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dataset = {
-    'Name': ['Raghib', 'Fahad', 'Abdul Rahman', 'Umar', 'Talha', 'Saif', 'Adina', 'Sara', 'Hafsa', 'Arwa'],
+    'Name': ['Saif', 'Fahad', 'Abdul Rahman', 'Umar', 'Talha', 'Raghib', 'Adina', 'Sara', 'Hafsa', 'Arwa'],
     'Sci': [50, 80, 40, 50, 40, 85, 84, 95, 95, 85],
     'Maths': [100, 58, 85, 48, 58, 75, 82, 99, 85, 95]
 }
