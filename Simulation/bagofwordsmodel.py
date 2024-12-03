3284ee368780824dc72a75a04cf27be319cd83d5
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-12-03 18:55:22 +0500

    Create bagofwordsmodel.py

---
+++ Simulation/bagofwordsmodel.py
@@ -0,0 +1,31 @@
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+paragraph = """Pakistan was established in 1947"""
+
+# Cleaning the texts
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+ps = PorterStemmer()
+wordnet = WordNetLemmatizer()
+sentences = nltk.sent_tokenize(paragraph)
+corpus = []
+
+for i in range(len(sentences)):
+    review = re.sub('[^a-zA-Z]', ' ', sentences[i])
+    review = review.lower()
+    review = review.split()
+    review = [wordnet.lemmatize(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
