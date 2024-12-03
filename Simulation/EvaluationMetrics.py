f027eece6a23256e8d89ae9e977dcaf39bfde0c8
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-12-03 17:59:07 +0500

    Update EvaluationMetrics.py

--- Simulation/EvaluationMetrics.py
+++ Simulation/EvaluationMetrics.py
@@ -1,6 +1,6 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, classification_report, precision_score, recall_score
 
 # Example data (using the Iris dataset)
 from sklearn.datasets import load_iris
